723e7d5935f53ae38c30e14ccb0c1fbcafa7ae35
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-06-16 15:05:40 +0200

    add browser

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 //import React from 'react'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <h1>home</h1>,
+  },
+   {
+    path:'/about',
+    element: (
+      <div>
+        <h1>about page</h1>
+      </div>
+    
+    ),
+  },
+]);
+
 const App = () => {
-  return (
-    <h1>Work</h1>
-  )
-}
+  return <RouterProvider router={router}/>;
+};
 
 export default App
