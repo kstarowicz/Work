e1c29ce2770efebb0a2be2a8cd752170d961d33e
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-06-23 21:51:53 +0200

    search

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,21 +2,25 @@ import styled from 'styled-components';
 //import React from 'react'
 
 
-const StyledBtn = styled.button`
-  font-size: 1,5rem;
-  background: red;
-  color: white;
-
-`
-
 const Landing = () => {
   return (
-    <div>
+    <Wrapper>
 
       <h1>Landing Page</h1>
-      <StyledBtn>aaa</StyledBtn>   
-    </div>
+      <div className="content">some content</div>
+    </Wrapper>
   )
 }
+
+const Wrapper =styled.div`
+  background: red;
+  h1{
+    color:white;
+  }
+  .content{
+    
+
+  }
+`;
 
 export default Landing
