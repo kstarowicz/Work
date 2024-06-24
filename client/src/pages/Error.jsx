1163716afa74856967ec726eede9ba690829caec
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-06-24 22:03:02 +0200

    Logo register

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -8,13 +8,24 @@ const Error = () => {
   const error = useRouteError();
 
   if(error.status === 404){
+    return(
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found"/>
+          <h3>Ohh! page not found</h3>
+          <p>We cant seem to find the page you are looking for</p>
+          <Link to='/dashboard'>back home</Link>
+        </div>
+      </Wrapper>
+    )
 
   }
 
   return (
     <Wrapper>
-      <h1>Error Page</h1>
-      <Link to = '/'>back home</Link>
+      <div>
+        <h3>something went wrong</h3>
+      </div>
     </Wrapper>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,41 @@
+
 import {Link} from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+//import { Logo } from '../assets/components/Logo'
+import { Logo } from '../components';
 //import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+        <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+          <input
+            type='text'
+            id='name'
+            name='name'
+            className='form-input'
+            defaultValue='karolina'
+            required
+          />
+        </div>
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
