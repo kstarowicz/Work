2ec02735880ff642f9dfa91ad328673013d1269a
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-06-24 12:59:51 +0200

    error 404

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,21 @@
 //import React from 'react'
 
 import { Link, useRouteError } from "react-router-dom"
+import Wrapper from "../assets/wrappers/ErrorPage"
+import img from '../assets/images/not-found.svg'
 
 const Error = () => {
   const error = useRouteError();
+
+  if(error.status === 404){
+
+  }
+
   return (
-    <div>
+    <Wrapper>
       <h1>Error Page</h1>
       <Link to = '/'>back home</Link>
-    </div>
+    </Wrapper>
   )
 }
 
