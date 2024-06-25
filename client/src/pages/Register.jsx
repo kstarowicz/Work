681fd962a8a536158ccf4aac536a37496228bbf9
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-06-25 16:24:52 +0200

    add new row form in register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -11,16 +11,32 @@ const Register = () => {
         <form className='form'>
         <Logo />
         <h4>Register</h4>
-        <FormRow type='text' name='name' defaultValue='karolina'/>
+
+        <FormRow 
+        type='text' 
+        name='name' 
+        defaultValue='karolina'
+        />
         <FormRow 
         type='text' 
         labelText='last name'
         name='lastName' 
         defaultValue='str'
         />
-        <FormRow type='text' name='name' defaultValue='karolina'/>
-        <FormRow type='text' name='name' defaultValue='karolina'/>
-        <FormRow type='text' name='name' defaultValue='karolina'/>
+        <FormRow 
+        type='text' 
+        name='location' 
+        defaultValue='earth'
+        />
+        <FormRow 
+        type='email' 
+        name='name' 
+        defaultValue='karolina@karolina.com'
+        />
+        <FormRow 
+        type='password' 
+        name='password' 
+        defaultValue='secret123'/>
 
         <button type='submit' className='btn btn-block'>
           submit
