4a9f88e541a8711fdae3d07e239dff42669c445d
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-08 23:06:42 +0200

    show-sidebar in samllsidebar

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,11 +1,25 @@
+import { FaTimes } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/SmallSidebar"
 import { useDashboardContext } from '../pages/DashboardLayout'
+import Logo from "./Logo";
 
 const SmallSidebar = () => {
   const data = useDashboardContext();
   console.log(data);
   return (
-    <Wrapper>SmallSidebar</Wrapper>
+    <Wrapper>
+      <div className='sidebar-container show-sidebar'>
+        <div className="content">
+          <button type="button" className="close-btn">
+            <FaTimes/>
+          </button>
+          <header>
+            <Logo />
+          </header>
+        </div>
+
+      </div>
+    </Wrapper>
   )
 }
 
