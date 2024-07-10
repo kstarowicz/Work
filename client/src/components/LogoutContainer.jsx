53333afa9ac4db8586eac11512181ec1a3d624af
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-10 11:28:17 +0200

    add LogoutContainer and setting state

--- client/src/components/LogoutContainer.jsx
+++ client/src/components/LogoutContainer.jsx
@@ -5,8 +5,25 @@ import { useDashboardContext } from '../pages/DashboardLayout';
 
 
 const LogoutContainer = () => {
+   const [showLogout, setShowLogout] = useState(false)
+   const {user, logoutUser} = useDashboardContext()
+
   return (
-    <div>LogoutContainer</div>
+    <Wrapper>
+        <button type='button' className='btn logout-btn'
+        onClick={()=>setShowLogout(!showLogout) }
+        >
+            <FaUserCircle/>
+            {user?.name}
+            <FaCaretDown/>
+        </button>
+        <div className={showLogout?'dropdown show-dropdown' : 'dropdown'}>
+            <button type='button' className='dropdown-btn'
+            onClick={logoutUser}>
+                logout
+            </button>
+        </div>
+    </Wrapper>
   )
 }
 
