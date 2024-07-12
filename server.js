95b34b9cf7c14a27d9a9ad0dfbccc1f650033c46
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-12 12:30:54 +0200

    add server post get ..

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ import { log } from 'console';
 import express from 'express'
 const app = express()
 
+app.use(express.json());
+
 app.get('/', (req,res) => {
     res.send('Hello World');
 });
+
+
+app.post('/', (req,res) =>{
+    console.log(req);
+    res.json({message:'data received', data:req.body })
+})
 
 app.listen(5100, () => {
     console.log('server running');
