a6f5a2dbf5db194270166c75e74704faf024aa1a
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-13 18:48:14 +0200

    add nanoid

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-import { log } from 'console';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express'
 const app = express()
 import morgan from 'morgan';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { nanoid } from 'nanoid';
+
+
+let jobs = [
+  { id: nanoid(), company: 'apple', position: 'front-end' },
+  { id: nanoid(), company: 'google', position: 'back-end' },
+]
 
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
@@ -21,6 +27,9 @@ app.post('/', (req,res) =>{
     console.log(req);
     res.json({message:'data received', data:req.body })
 })
+
+
+app.get('/api/v1/jobs')
 
 const port = process.env.PORT || 5100
 
