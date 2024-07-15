e24d03803a013e30408d6b4d7943c0828b34df32
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-15 23:44:30 +0200

    update job controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -13,8 +13,8 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-  const job = await Job.create({ company, position })
+  
+  const job = await Job.create(req.body)
   res.status(201).json({ job })
 };
 
