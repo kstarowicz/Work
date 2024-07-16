40c52a2d5817d37b6b6fcdae61aab6b5886ce9f2
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-16 17:34:14 +0200

    fix code update

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,12 +1,6 @@
 import Job from '../models/JobModel.js';
 import express from 'express';
-import { nanoid } from 'nanoid';
-//import jobRouter from './routers/jobRouter.js';
 
-let jobs = [
-  { id: nanoid(), company: 'apple', position: 'front-end' },
-  { id: nanoid(), company: 'google', position: 'back-end' },
-]
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
@@ -29,20 +23,17 @@ export const getJob = async (req, res) => {
 };
 
 export const updateJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    res.status(400).json({ msg: 'please provide company and position' });
-    return ;
-  }
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  if (!updatedJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: 'job modified', job });
+  res.status(200).json({ job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
