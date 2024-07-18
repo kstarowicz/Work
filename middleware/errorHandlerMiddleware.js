5822ebe87717e4dba922a747e233509b700764f3
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-18 14:19:43 +0200

    add error typ in customError

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -1,5 +1,4 @@
 import { StatusCodes } from 'http-status-codes';
-
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
