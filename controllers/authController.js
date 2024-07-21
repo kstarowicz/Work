b78c446f2d1e1b143390863ba146cfc9ccb039ae
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-21 19:16:26 +0200

    authController autenticated error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { hashPassword } from '../utils/passwordUtils.js'
+import { UnauthenticatedError } from '../errors/customError.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -14,5 +15,11 @@ export const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ msg:'user created' });
 };
 export const login = async (req, res) => {
-  res.send('login');
+  // check if user exists
+  // check if password is correct
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) throw new UnauthenticatedError('invalid credentials');
+
+  res.send('login route');
 };
