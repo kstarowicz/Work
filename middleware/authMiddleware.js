36016c0055380baf8983699d457efd25d2351a13
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-22 20:39:27 +0200

    Verify JWT

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,12 @@
-
+import { UnauthenticatedError } from '../errors/customError.js';
 
 export const authenticateUser = async (req, res, next) => {
-  console.log('auth middleware');
+  const { token } = req.cookies;
+  if (!token) {
+    throw new UnauthenticatedError('authentication invalid');
+  }
   next();
 };
+
+
 
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 
+import cookieParser from 'cookie-parser';
+
 //import { validateTest } from './middleware/validationMiddleware.js';
 
 //routers
@@ -20,6 +22,7 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get('/', (req,res) => {
