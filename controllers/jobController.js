e872b92f0ed60428f7a79dc67a33821b86424587
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-24 19:45:37 +0200

    add user to job routres

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,11 +4,13 @@ import { NotFoundError } from '../errors/customError.js';
 import express from 'express';
 
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({});
+
+  const jobs = await Job.find({createdBy: req.user.userID});
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userID
      const job = await Job.create( req.body )
      res.status(StatusCodes.CREATED).json({ job })
 };
@@ -22,13 +24,11 @@ export const updateJob = async (req, res) => {
   const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
-
   res.status(StatusCodes.OK).json({ msg: 'job modified', job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const removedJob = await Job.findByIdAndDelete(req.params.id);
-
   res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob });
 };
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,16 +3,14 @@ import { verifyJWT } from '../utils/tokenUtils.js';
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
-    console.log('Token from cookies:', token);
   if (!token) throw new UnauthenticatedError('authentication invalid');
   
   try {
     const { userId, role } = verifyJWT(token);
-    console.log('Decoded token:', { userId, role });
+
     req.user = { userId, role };
     next();
   } catch (error) {
-    console.error('Token verification failed:', error);
     throw new UnauthenticatedError('authentication invalid');
   }
 };
