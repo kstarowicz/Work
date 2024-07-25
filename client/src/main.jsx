8c377e2beb3c3143bb1d9cd3f9c481d306fcc5fe
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-25 23:22:17 +0200

    proxy setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+fetch('http://localhost:5100/api/v1/test')
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
