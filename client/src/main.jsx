6ed534fffb5380cebfa9a243998101665e6c139d
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-25 23:49:11 +0200

    axios start

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios';
 
+const data = await axios.get('/api/v1/test');
+console.log(data);
 
 fetch('http://localhost:5100/api/v1/test')
   .then((res) => res.json())
   .then((data) => console.log(data));
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
