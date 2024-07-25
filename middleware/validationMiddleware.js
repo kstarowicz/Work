5ea585a7603034294da0b17aceb83b231f79f873
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-25 11:24:02 +0200

    Logout

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -12,12 +12,15 @@ const withValidationErrors = (validateValues) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         const errorMessages = errors.array().map((error) => error.msg);
+
         if (errorMessages[0].startsWith('no job')) {
           throw new NotFoundError(errorMessages);
         }
          if (errorMessages[0].startsWith('not authorized')) {
           throw new UnauthorizedError('not authorized to access this route');
       }
+      throw new BadRequestError(errorMessages);
+    }
       next();
     },
   ];
@@ -44,7 +47,7 @@ export const validateIdParam = withValidationErrors([
 
     const isAdmin = req.user.role === 'admin'
     const isOwner = req.user.userId === job.createdBy.toString()
-    if(!isAdmin  && !isOwner)
+    if(!isAdmin && !isOwner)
        throw new UnauthorizedError('not authorized to access this route');
 
   }),
