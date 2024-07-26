f524cebe8b24d9f6b853a3cd9651d70a614a2ed9
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-27 01:26:02 +0200

    logout fix

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,7 +1,8 @@
-import { Outlet, redirect, useLoaderData } from "react-router-dom"
+import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom"
 import Wrapper from "../assets/wrappers/Dashboard"
 import { BigSidebar, Navbar, SmallSidebar } from "../components"
 import { createContext, useContext, useState } from "react"
+import customFetch from "../utils/customFetch"
 
 
 export const loader = async () => {
@@ -18,7 +19,7 @@ const DashboardContext = createContext()
 const DashboardLayout = ({ isDarkThemeEnabled }) => {
 
   const {user} = useLoaderData();
-  
+  const navigate = useNavigate();
   const [showSidebar,setShowSidebar] = useState(false);
   const [isDarkTheme,setIsDarkTheme] = useState(isDarkThemeEnabled);
 
@@ -35,7 +36,9 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
   };
 
   const logoutUser = async () => {
-    console.log('logout user')
+    navigate('/')
+    await customFetch.get('/auth/logout')
+    toast.success('Logging out ...')
   };
 
   return (
