5fb9f03e72964ed3ba51dcf8517bda2da91ffdbe
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-07-27 00:43:41 +0200

    Action User

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Link, Form, redirect, useNavigation } from 'react-router-dom';
+import { Link, Form, redirect, useNavigation, useActionData } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
 import customFetch from '../utils/customFetch';
@@ -10,17 +10,24 @@ import { toast } from 'react-toastify';
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  const errors = {msg:''}
+  if(data.password.length < 3){
+    errors.msg = 'password too short'
+    return errors;
+  }
   try {
     await customFetch.post('/auth/login', data);
     toast.success('Login successful');
     return redirect('/dashboard');
   } catch (error) {
-    toast.error(error?.response?.data?.msg);
+   // toast.error(error?.response?.data?.msg);
+   errors.msg = error?.response?.data?.msg;
     return error;
   }
 };
 
 const Login = () => {
+  const errors =useActionData()
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   return (
@@ -28,6 +35,8 @@ const Login = () => {
      <Form method='post' className='form'>
       <Logo />
        <h4>login </h4>
+       {errors?.msg && <p style={ {color:'red'} }>{ errors.msg }</p>}
+       <p></p>
        <FormRow type='email' name='email'
        defaultValue='karolina@karolina.com' />
 
