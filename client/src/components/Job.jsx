f1dceaa71eab3cce2a31afd76f8e5d7afd6d1703
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-08-02 20:56:16 +0200

    EditJob function

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,8 +1,7 @@
 import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
 import { Link, Form } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Job';
-import JobInfo from './JobIn
-fo';
+import JobInfo from './JobInfo';
 import day from 'dayjs';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 day.extend(advancedFormat);
