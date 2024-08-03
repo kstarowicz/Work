83ac6f934a22df61ed3c766b69dc51645cc94ff9
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-08-03 20:43:50 +0200

    EditJob action

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -8,15 +8,24 @@ import customFetch from '../utils/customFetch';
 
 export const loader = async ({params}) => {
  try {
-   const {data} = await customFetch.get(`/job/${params.id}`)
+   const {data} = await customFetch.get(`/jobs/${params.id}`)
    return data
  } catch (error) {
   toast.error(error?.response?.data?.msg);
   return redirect('/dashboard/all-jobs');
  }
 };
-export const action = async () => {
-  return null;
+export const action = async ({request,params}) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  try {
+    await customFetch.patch(`/jobs/${params.id}`, data)
+    toast.success('Job edited successfully')
+    return redirect('/dashboard/all-jobs')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error
+  }
 };
 
 const EditJob = () => {
