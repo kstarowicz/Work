baa6b91ffed65127fb5eb32a94df539e60817569
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-08-06 00:37:01 +0200

    multerMiddl2

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,14 +11,16 @@ export const getCurrentUser = async (req, res) => {
 };
 
 export const getApplicationStats = async (req, res) => {
-  const users = await User.countDocuments()
-  const jobs = await Job.countDocuments()
+  const users = await User.countDocuments();
+  const jobs = await Job.countDocuments();
   res.status(StatusCodes.OK).json({ users, jobs });
 };
 
 export const updateUser = async (req, res) => {
+  console.log(req.file);
   const obj = {...req.body};
   delete obj.password;
+
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj)
   res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
 import {
-  getCurrentUser,
   getApplicationStats,
+  getCurrentUser,
   updateUser,
 } from '../controllers/userController.js';
 import {validateUpdateUserInput} from '../middleware/validationMiddleware.js'
 import {authorizePermissions} from '../middleware/authMiddleware.js'
+import upload from '../middleware/multerMiddleware.js';
 
 const router = Router();
 
@@ -15,5 +16,10 @@ router.get('/admin/app-stats', [
   authorizePermissions('admin'), 
   getApplicationStats,
 ]);
-router.patch('/update-user', validateUpdateUserInput, updateUser);
+router.patch(
+  '/update-user',
+  upload.single('avatar'),
+  validateUpdateUserInput,
+  updateUser
+);
 export default router;
