5d0119c4ebad5883f2681a640504cdd6d013b82c
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-08-06 21:22:49 +0200

    loginDemoUser add

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Link, Form, redirect} from 'react-router-dom';
+import { Link, Form, redirect, useNavigate} from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo, SubmitBtn } from '../components';
 import customFetch from '../utils/customFetch';
@@ -22,6 +22,21 @@ export const action = async ({ request }) => {
 };
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const loginDemoUser = async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'secret123',
+    };
+    try {
+      await customFetch.post('/auth/login', data);
+      toast.success('Take a test drive');
+      navigate('/dashboard');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+  };
   return (
     <Wrapper>
      <Form method='post' className='form'>
@@ -33,7 +48,7 @@ const Login = () => {
        <FormRow type='passeord' name='passeord'
        defaultValue='secret123' />
        <SubmitBtn/>
-       <button type='button' className='btn btn-block'>
+       <button type='button' className='btn btn-block' onClick={loginDemoUser}>
         explore the app
        </button>
 
