11f4cee2e979761efce9bfab77cfd781c4f7bc1e
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-08-07 21:27:28 +0200

    Stats add function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,6 +30,7 @@ import { action as editJobAction} from './pages/EditJob';
 import { action as deleteJobAction} from './pages/DeleteJob';
 import { loader as adminLoader} from './pages/Admin';
 import { action as profileAction } from './pages/Profile';
+import { loader as statsLoader } from './pages/Stats';
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -76,6 +77,7 @@ const router = createBrowserRouter([
       {
         path:'stats',
         element:< Stats />,
+        loader: statsLoader,
       },
             {
         path:'all-jobs',
---
+++ client/src/components/ChartsContainer.jsx
@@ -0,0 +1,8 @@
+
+const ChartsContainer = () => {
+  return (
+    <h1>ChartsContainer</h1>
+  )
+}
+
+export default ChartsContainer;
