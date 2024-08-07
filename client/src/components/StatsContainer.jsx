b7ef309623382476e408038396b476bfd49b3e71
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-08-07 21:55:00 +0200

    Stats fix

--- client/src/components/StatsContainer.jsx
+++ client/src/components/StatsContainer.jsx
@@ -1,8 +1,38 @@
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from 'react-icons/fa';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import StatItem from './StatItem';
 
-const StatsContainer = () => {
+
+const StatsContainer = ({ defaultStats }) => {
+  const stats = [
+    {
+      title: 'pending applications',
+      count: defaultStats?.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: '#f59e0b',
+      bcg: '#fef3c7',
+    },
+    {
+      title: 'interviews scheduled',
+      count: defaultStats?.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: '#647acb',
+      bcg: '#e0e8f9',
+    },
+    {
+      title: 'jobs declined',
+      count: defaultStats?.declined || 0,
+      icon: <FaBug />,
+      color: '#d66a6a',
+      bcg: '#ffeeee',
+    },
+  ];
   return (
-    <h1>StatsContainer</h1>
-  )
-}
-
-export default StatsContainer
+    <Wrapper>
+      {stats.map((item) => {
+        return <StatItem key={item.title} {...item} />;
+      })}
+    </Wrapper>
+  );
+};
+export default StatsContainer;
