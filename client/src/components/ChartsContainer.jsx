96d3d59ebe9f657c197ebc0a967b4622c7bfc33b
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-08-07 22:39:21 +0200

    add folder

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 
-const ChartsContainer = () => {
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
+
   return (
-    <h1>ChartsContainer</h1>
-  )
-}
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
+};
 
 export default ChartsContainer;
